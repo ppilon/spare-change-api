c056a56949a5fcfa2304ae20a664dbdc2c79ccaa
ppilon <pilon24@icloud.com>
2018-04-13 15:26:52 -0400

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # RESTful routes
+  resources :users, only: %i[index show update]
+
+  # Custom routes
+  post '/sign-up' => 'users#signup'
+  post '/sign-in' => 'users#signin'
+  delete '/sign-out' => 'users#signout'
+  patch '/change-password' => 'users#changepw'
+end
